3fdfa185eecbe494955beb577e1192293856def5
Lucas Probst <lucasprobst@rede.ulbra.br>
2025-03-24 23:32:28 -0300

    subindo url admin

--- projeto_curso/urls.py
+++ projeto_curso/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from recipes.views import home
+from django.contrib import admin
 
 urlpatterns = [
     path('', home),
     path('', include('recipes.urls')),
+     path('admin/', admin.site.urls),
 ]
